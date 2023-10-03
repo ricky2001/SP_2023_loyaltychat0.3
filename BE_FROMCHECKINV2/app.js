6fa5141e10c81204b161d78deeb1c06ca3c89a46
Nuntawat6331305014 <6331305014@lamduan.mfu.ac.th>
2023-10-03 10:31:01 +0700

    downdate

--- BE_FROMCHECKINV2/app.js
+++ BE_FROMCHECKINV2/app.js
@@ -2,30 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const app = express();
-const http = require('http');
-const { generateText } = require('./controllers/openai');
 
 // Routes
 const authRoutes = require("./routes/auth");
-
-const server = http.createServer(async (req, res) => {
-  if (req.url === '/generate-text') {
-    try {
-      // Generate text using the OpenAI API
-      const generatedText = await generateText();
-
-      // Send the generated text as the HTTP response
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(generatedText);
-    } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Error generating text');
-    }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
-});
 
 // Middlewares
 app.use(bodyParser.json());
