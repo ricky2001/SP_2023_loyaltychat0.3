3b38f92abb7d18520d3ba0f53b65b6439bc4120a
Nuntawat6331305014 <6331305014@lamduan.mfu.ac.th>
2023-10-03 10:10:05 +0700

    gan

--- BE_FROMCHECKINV2/controllers/openai.js
+++ BE_FROMCHECKINV2/controllers/openai.js
@@ -1,25 +1,69 @@
-const { Configuration, OpenAIApi } = require("openai");
+const axios = require('axios');
+require('dotenv').config();
 
-const config = new Configuration({
-  apiKey: "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",
-});
+const generateText = async () => {
+  try {
+    // Get your OpenAI API key from environment variables
+    const apiKey = process.env.OPENAI_API_KEY;
 
-const openai = new OpenAIApi(config);
+    // Specify the OpenAI API endpoint
+    const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
 
-exports.openai = (req, res) => {
-  const { message } = req.body;
+    // Create a text prompt for the AI to complete
+    const prompt = 'hi';
 
-  const response = openai.createChatCompletion(
-    {
-      model: 'gpt-3.5-turbo',
-      stream: true,
-      prompt: {message},
-      max_tokens: 256,
-      temperature: 0.7,
-    });
+    // Make a POST request to the OpenAI API
+    const response = await axios.post(
+      apiUrl,
+      {
+        prompt,
+        max_tokens: 50, // Adjust this to control the length of generated text
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+      }
+    );
 
-  console.log(response.data);
-  
+    // Extract the generated text from the response
+    const generatedText = response.data.choices[0].text;
+
+    return generatedText;
+  } catch (error) {
+    console.error('Error:', error);
+    throw new Error('Error generating text');
+  }
 };
+
+module.exports = {
+  generateText,
+};
+
+
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const config = new Configuration({
+//   apiKey: "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",
+// });
+
+// const openai = new OpenAIApi(config);
+
+// exports.openai = (req, res) => {
+//   const { message } = req.body;
+
+//   const response = openai.createChatCompletion(
+//     {
+//       model: 'gpt-3.5-turbo',
+//       stream: true,
+//       prompt: {message},
+//       max_tokens: 256,
+//       temperature: 0.7,
+//     });
+
+//   console.log(response.data);
+  
+// };
 
 
