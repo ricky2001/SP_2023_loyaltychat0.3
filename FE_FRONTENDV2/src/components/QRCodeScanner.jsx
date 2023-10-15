01c1972dae1e7d38e5970a4277e337d1da5f5b9b
JIRAWAT RATSAMEE <6331305002@lamduan.mfu.ac.th>
2023-10-15 14:46:30 +0700

    scanqrnew

--- FE_FRONTENDV2/src/components/QRCodeScanner.jsx
+++ FE_FRONTENDV2/src/components/QRCodeScanner.jsx
@@ -1,145 +1,202 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Html5Qrcode } from 'html5-qrcode';
-import { useSelector, useDispatch } from 'react-redux'
-import { scanqrcode } from '@/stores/api/index'
+// import React, { useState, useEffect } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { Html5Qrcode } from 'html5-qrcode';
+// import { useSelector, useDispatch } from 'react-redux'
+// import { scanqrcode } from '@/stores/api/index'
 
-function QRCodeScanner() {
-  const [scanResultWebCam, setScanResultWebCam] = useState('');
-  const navigate = useNavigate();
-  let html5QrCodeScanner = null;
+// function QRCodeScanner() {
+//   const [scanResultWebCam, setScanResultWebCam] = useState('');
+//   const navigate = useNavigate();
+//   let html5QrCodeScanner = null;
   
-  const [isScanning, setIsScanning] = useState(false);
-  const [facingMode, setFacingMode] = useState('environment'); // 'environment' for rear camera, 'user' for front camera
+//   const [isScanning, setIsScanning] = useState(false);
+//   const [facingMode, setFacingMode] = useState('environment'); // 'environment' for rear camera, 'user' for front camera
 
-  const handleErrorWebCam = (error) => {
-    console.error(error);
-  };
+//   const handleErrorWebCam = (error) => {
+//     console.error(error);
+//   };
 
-  const startScanning = async () => {
-    try {
-      // Specify the id of the container element
-      const containerId = 'qr-code-scanner-container';
+//   const startScanning = async () => {
+//     try {
+//       // Specify the id of the container element
+//       const containerId = 'qr-code-scanner-container';
 
-      // Create the Html5Qrcode instance
+//       // Create the Html5Qrcode instance
 
-      if (!html5QrCodeScanner) {
-        html5QrCodeScanner = new Html5Qrcode(containerId);
-      }
+//       if (!html5QrCodeScanner) {
+//         html5QrCodeScanner = new Html5Qrcode(containerId);
+//       }
 
-      await html5QrCodeScanner.start(
-        { facingMode: facingMode }, // specify the camera configuration
-        {
-          fps: 10,
-          qrbox: 250,
-        },
-        (qrCodeMessage) => {
-          // Success callback function
-          setScanResultWebCam(qrCodeMessage);
-        }
-      );
+//       await html5QrCodeScanner.start(
+//         { facingMode: facingMode }, // specify the camera configuration
+//         {
+//           fps: 10,
+//           qrbox: 250,
+//         },
+//         (qrCodeMessage) => {
+//           // Success callback function
+//           setScanResultWebCam(qrCodeMessage);
+//         }
+//       );
 
-      try {
-        const response = await fetch(scanqrcode(), {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ qrCodeMessage }),
-        });if (response.ok) {
-          // Handle successful API response here
-          const data = await response.json();
-          console.log('API Response:', data);
-        } else {
-          // Handle API error here
-          console.error('API Error:', response.status);
-        }
-      } catch (error) {
-        console.error('Error sending POST request:', error);
-      }
+//       try {
+//         const response = await fetch(scanqrcode(), {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({ qrCodeMessage }),
+//         });if (response.ok) {
+//           // Handle successful API response here
+//           const data = await response.json();
+//           console.log('API Response:', data);
+//         } else {
+//           // Handle API error here
+//           console.error('API Error:', response.status);
+//         }
+//       } catch (error) {
+//         console.error('Error sending POST request:', error);
+//       }
     
 
-      setIsScanning(true);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//       setIsScanning(true);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  const [isNavigating, setIsNavigating] = useState(false);
+//   const [isNavigating, setIsNavigating] = useState(false);
 
-  const navigateWithGuard = async (path) => {
-    if (!isNavigating) {
-      setIsNavigating(true);
-      try {
-        await navigate(path);
-      } finally {
-        setIsNavigating(false);
-      }
-    }
-  };
+//   const navigateWithGuard = async (path) => {
+//     if (!isNavigating) {
+//       setIsNavigating(true);
+//       try {
+//         await navigate(path);
+//       } finally {
+//         setIsNavigating(false);
+//       }
+//     }
+//   };
 
-  // In your stopScanning function, use navigateWithGuard instead of navigate
-  const stopScanning = () => {
-    if (isScanning && html5QrCodeScanner) {
-      html5QrCodeScanner.stop().then(() => {
-        html5QrCodeScanner.clear();
-        setScanResultWebCam('');
-        setIsScanning(false);
+//   // In your stopScanning function, use navigateWithGuard instead of navigate
+//   const stopScanning = () => {
+//     if (isScanning && html5QrCodeScanner) {
+//       html5QrCodeScanner.stop().then(() => {
+//         html5QrCodeScanner.clear();
+//         setScanResultWebCam('');
+//         setIsScanning(false);
 
-        navigateWithGuard('/calendar');
-      });
-    }
-  };
+//         navigateWithGuard('/calendar');
+//       });
+//     }
+//   };
 
-  // const [scanqrcode, setscanqrcode] = useState([]);
-  // const dispatch = useDispatch();
+//   // const [scanqrcode, setscanqrcode] = useState([]);
+//   // const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const fectchdata = dispatch(scanqrcode())
-  //   console.log(fectchdata);
-  // }, [dispatch])
+//   // useEffect(() => {
+//   //   const fectchdata = dispatch(scanqrcode())
+//   //   console.log(fectchdata);
+//   // }, [dispatch])
 
 
-  const switchCamera = () => {
-    // Toggle between 'environment' and 'user' facing modes
-    const newFacingMode = facingMode === 'environment' ? 'user' : 'environment';
-    setFacingMode(newFacingMode);
+//   const switchCamera = () => {
+//     // Toggle between 'environment' and 'user' facing modes
+//     const newFacingMode = facingMode === 'environment' ? 'user' : 'environment';
+//     setFacingMode(newFacingMode);
 
-    stopScanning(); // Stop scanning when switching cameras
+//     stopScanning(); // Stop scanning when switching cameras
 
-    // Start scanning with the new facing mode
-    startScanning();
-  };
+//     // Start scanning with the new facing mode
+//     startScanning();
+//   };
+
+//   useEffect(() => {
+//     startScanning();
+
+//     return () => {
+//       if (isScanning) {
+//         stopScanning(); // Stop scanning and release resources when the component unmounts
+//       }
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>QR Code Scanner</h1>
+//       <div id="qr-code-scanner-container"></div>
+//       <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
+
+//       <button onClick={stopScanning} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-xl">
+//         Stop Scanning
+//       </button>
+
+//       <button onClick={switchCamera} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">
+//         Switch Camera
+//       </button>
+
+//       <Link to="/calendar">
+//         <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-xl">Go Back to Another Page</button>
+//       </Link>
+//     </div>
+//   );
+// }
+
+// export default QRCodeScanner;
+import React, { useEffect, useRef, useState } from 'react';
+import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
+import { Link } from 'react-router-dom';
+
+const QRCodeScanner = () => {
+  const videoRef = useRef(null);
+  const codeReader = new BrowserMultiFormatReader();
+  const [scannedData, setScannedData] = useState('');
+  // const [scanning, setScanning] = useState(true);
 
   useEffect(() => {
-    startScanning();
+    // Initialize scanner and video input devices when the component mounts
+    codeReader
+      .listVideoInputDevices()
+      .then((videoInputDevices) => {
+        if (videoInputDevices.length > 0) {
+          codeReader.decodeFromVideoDevice(
+            videoInputDevices[0].deviceId,
+            videoRef.current,
+            (result, error) => {
+              if (result) {
+                setScannedData(result.getText());
+                // setScanning(false);
+              } else if (error && error instanceof NotFoundException) {
+                console.log('No QR code found.');
+              }
+            }
+          );
+        } else {
+          console.error('No video input devices found.');
+        }
+      })
+      .catch((err) => {
+        console.error('Error accessing video devices:', err);
+      });
 
+    // Cleanup function: Reset the scanner when the component unmounts
     return () => {
-      if (isScanning) {
-        stopScanning(); // Stop scanning and release resources when the component unmounts
-      }
+      codeReader.reset();
     };
   }, []);
 
   return (
     <div>
-      <h1>QR Code Scanner</h1>
-      <div id="qr-code-scanner-container"></div>
-      <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
-
-      <button onClick={stopScanning} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-xl">
-        Stop Scanning
-      </button>
-
-      <button onClick={switchCamera} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">
-        Switch Camera
-      </button>
-
-      <Link to="/calendar">
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-xl">Go Back to Another Page</button>
-      </Link>
+      <h2>QR Code Scanner</h2>
+      
+        <div>
+          <video ref={videoRef} playsInline autoPlay muted />
+          <p>Scanned QR Code: {scannedData}</p>
+          <Link to="/calendar"><button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-xl">Go Back to Another Page</button></Link>
+        </div>
+       
     </div>
   );
-}
+};
 
 export default QRCodeScanner;
