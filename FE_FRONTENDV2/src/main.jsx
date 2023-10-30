bdf7519cf2dbe88e0d010237cf1a52b081d54383
JIRAWAT RATSAMEE <6331305002@lamduan.mfu.ac.th>
2023-10-30 22:26:26 +0700

    QRCodeGenerator.

--- FE_FRONTENDV2/src/main.jsx
+++ FE_FRONTENDV2/src/main.jsx
@@ -20,6 +20,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import  ProtectedRoute from './routers/protectRouter';
 import QRCodeScanner from './components/QRCodeScanner.jsx';
 import Form from './pages/Form.jsx'
+import QRcodegenerator from './pages/QRcodegenerator.jsx'
 
 const router = createBrowserRouter([
   {
@@ -52,6 +53,9 @@ const router = createBrowserRouter([
   },{
     path:"/form",
     element:<ProtectedRoute component={<Form/>} />,
+},{
+  path:"/QRcodegenerator",
+  element:<ProtectedRoute component={<QRcodegenerator/>} />,
 }
 ]);
 
---
+++ FE_FRONTENDV2/src/pages/QRcodegenerator.jsx
@@ -0,0 +1,46 @@
+import QRCode from 'qrcode'
+import { useState } from 'react'
+// import './App.css'
+import './QRcodegenerator.css';
+
+function QRcodegenerator() {
+	const [url, setUrl] = useState('')
+	const [qr, setQr] = useState('')
+
+	const GenerateQRCode = () => {
+		QRCode.toDataURL(url, {
+			width: 800,
+			margin: 2,
+			// color: {
+			// 	dark: '#335383FF',
+			// 	light: '#EEEEEEFF'
+			// }
+		}, (err, url) => {
+			if (err) return console.error(err)
+
+			console.log(url)
+			setQr(url)
+		})
+	}
+
+	return (
+        
+		<div className="QRcodegenerator">
+			<div>
+            <h1>QR Generator</h1>
+            </div>
+			<input 
+				type="text"
+				placeholder="e.g. https://google.com"
+				value={url}
+				onChange={e => setUrl(e.target.value)} />
+			<button onClick={GenerateQRCode}>Generate</button>
+			{qr && <>
+				<img src={qr} />
+				<a href={qr} download="qrcode.png">Download</a>
+			</>}
+		</div>
+	)
+}
+
+export default QRcodegenerator
