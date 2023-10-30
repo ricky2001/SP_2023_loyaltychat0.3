1703924286dc5d6457ffdce3ddc076de01192189
Ricky <57288742+ricky2001@users.noreply.github.com>
2023-10-30 19:32:59 +0700

    Update Dashboard.jsx

--- FE_FRONTENDV2/src/pages/Dashboard.jsx
+++ FE_FRONTENDV2/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import Carousel from '@/components/CarouselDashboard';
 import Recent from '@/components/Recents';
 import Describtions from '@/components/Describtions';
 import Newsfeed from '@/components/Newsfeed';
+import Chatbot from '../components/Chatbot';
 import ChatBox from '../components/ChatBox/Chatbox';
 
 function Dashboard() {
@@ -13,10 +14,11 @@ function Dashboard() {
             <div className='h-full w-full '>
                 <div className="flex flex-col justify-center items-center">
                     <Carousel />
-                    <Recent/>
-                    {/* <Newsfeed /> */}
+                    {/* <Recent/> */}
+                    <Newsfeed />
                     {/* <Describtions /> */}
-                    {/* <ChatBox /> */}
+                    <ChatBox />
+                    {/* <Chatbot /> */}
                 </div>
             </div>
         </Base>
