046c526a263224e7df3965214edc17ba05c231bb
ricky2001 <cp96467@gmail.com>
2023-11-09 13:35:33 +0700

    up

--- FE_FRONTENDV2/src/components/QRCodeScanner.jsx
+++ FE_FRONTENDV2/src/components/QRCodeScanner.jsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-
-import React, { useEffect, useRef, useState } from 'react';
-import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
-import { Link } from 'react-router-dom';
-import {scanqrcode} from '@/stores/api/index'
-=======
 // import React, { useState } from 'react';
 // import { useNavigate } from 'react-router-dom'; // Import useNavigate
 // import { QrReader } from 'react-qr-reader';
@@ -225,63 +218,11 @@ import {scanqrcode} from '@/stores/api/index'
 import React, { useEffect, useRef, useState } from 'react';
 import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
 import { Link } from 'react-router-dom';
->>>>>>> 0d40e894d826878fa844ab596fef0d075ea27311
 
 const QRCodeScanner = () => {
   const videoRef = useRef(null);
   const codeReader = new BrowserMultiFormatReader();
   const [scannedData, setScannedData] = useState('');
-<<<<<<< HEAD
-  // const [scanning, setScanning] = useState(true);
-
-  useEffect(() => {
-    // Initialize scanner and video input devices when the component mounts
-    codeReader
-      .listVideoInputDevices()
-      .then((videoInputDevices) => {
-        if (videoInputDevices.length > 0) {
-          codeReader.decodeFromVideoDevice(
-            videoInputDevices[0].deviceId,
-            videoRef.current,
-            (result, error) => {
-              if (result) {
-                setScannedData(result.getText());
-                // setScanning(false);
-              } else if (error && error instanceof NotFoundException) {
-                console.log('No QR code found.');
-              }
-            }
-          );
-        } else {
-          console.error('No video input devices found.');
-        }
-      })
-      .catch((err) => {
-        console.error('Error accessing video devices:', err);
-      });
-
-    // Cleanup function: Reset the scanner when the component unmounts
-    return () => {
-      codeReader.reset();
-    };
-  }, []);
-
-  return (
-    <div>
-      <h2>QR Code Scanner</h2>
-      
-        <div>
-          <video ref={videoRef} playsInline autoPlay muted />
-          <p>Scanned QR Code: {scannedData}</p>
-          <Link to="/calendar"><button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-xl">Go Back to Another Page</button></Link>
-        </div>
-       
-    </div>
-  );
-};
-
-export default QRCodeScanner;
-=======
   const [selectedDeviceIndex, setSelectedDeviceIndex] = useState(0);
   const [videoInputDevices, setVideoInputDevices] = useState([]);
 
@@ -353,4 +294,3 @@ export default QRCodeScanner;
 
 
 
->>>>>>> 0d40e894d826878fa844ab596fef0d075ea27311
