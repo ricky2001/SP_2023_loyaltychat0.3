cc51e63f201a5958a35faa7f36c610c27d96c5ce
ricky2001 <cp96467@gmail.com>
2023-11-20 01:29:44 +0700

    update deploy

--- BE_FROMCHECKINV2/app.js
+++ BE_FROMCHECKINV2/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const app = express();
+const functions = require('firebase-functions');
 
 // Routes
 const authRoutes = require("./routes/auth");
 
-// Middlewares
 app.use(bodyParser.json());
 app.use(cors()); 
 
@@ -20,3 +20,39 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Start server : ${port}`);
 });
+
+
+
+//for deploy
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require('cors');
+// const app = express();
+// const functions = require('firebase-functions');
+// const authRoutes = require("./routes/auth");
+
+// app.use(bodyParser.json());
+// app.use(cors()); 
+
+// // Routes
+// app.use("/api", authRoutes);
+
+// // exports.api = functions.https.onRequest(async (req, res) => {
+// //   try {
+   
+// //     // Middlewares
+
+// // // PORT
+// // // const port = 3000;
+
+// // // // Starting a server
+// // // app.listen(port, () => {
+// // //   console.log(`Start server : ${port}`);
+// // // });
+// //   } catch (error) {
+// //     console.error('Error:', error);
+// //     res.status(500).send('Internal Server Error');
+// //   }
+// // });
+
+// exports.api = functions.https.onRequest(app);
