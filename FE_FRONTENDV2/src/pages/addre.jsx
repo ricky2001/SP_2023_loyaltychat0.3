4d8464a754e870674bd4a85e0ef2a509ff291c86
JIRAWAT RATSAMEE <6331305002@lamduan.mfu.ac.th>
2023-11-26 19:38:42 +0700

    a

--- FE_FRONTENDV2/src/pages/addre.jsx
+++ FE_FRONTENDV2/src/pages/addre.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { IoChevronBackSharp } from 'react-icons/io5';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate} from 'react-router-dom';
 import Base from '@/layouts/base.jsx'
 function addre() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    id: '',
+    name: '',
+    details: '',
+    price: '',
+    total: '',
+  });
+
+
+
   const [images, setImages] = useState([]);
   const [imageURLs, setImageURLs] = useState([]);
-  // const [isPopupOpen, setPopupOpen] = useState(false);
-  // const [isEditOpen, setEditOpen] = useState(false);
-  // const [isPopupOpenForm, setPopupOpenForm] = useState(false);
-
-
-
-
-
-
-
 
   useEffect(() => {
     if (images.length < 1) return;
@@ -26,9 +27,59 @@ function addre() {
   function onImageChange(e) {
     setImages([...e.target.files]);
   }
-  console.log("Images : ", images);
-  console.log("imageURLs : ", imageURLs);
+  // console.log("Images : ", images);
+  // console.log("imageURLs : ", imageURLs);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // const numericValue = value.replace(/[^0-9]/g, '');
+    setFormData({
+      formData,
+      [name]: value,
+    });
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("data : ", formData);
+    console.log("Images : ", images);
+    alert('Update Form button clicked!');
+    // Prepare data to send to the backend
+    const dataToSend = {
+      formData,
+      images,
+
+    };
+    setFormData({
+      id: '',
+      name: '',
+      details: '',
+      price: '',
+      total: '',
+    });
+    navigate('/point');
+    
+    try {
+      // Send data to the backend
+      const response = await fetch('your_backend_api_endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      // Handle the response from the backend if needed
+      const responseData = await response.json();
+      console.log('Success:', responseData);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    
+  };
   return (
     <Base>
       <br />
@@ -48,30 +99,30 @@ function addre() {
               <h3 >ADD REWARD</h3>
             </div>
             <br /><br />
-            <form>
+            <form onSubmit={handleSubmit}>
               <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <label htmlFor="id">ID : </label>&nbsp;&nbsp;
-                <input type="text" name='id' className='form-control' />
+                <input type="text" name='id' className='form-control' value={formData.id} onChange={handleChange} />
               </div>
               <br />
               <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <label htmlFor="name">Name : </label>&nbsp;&nbsp;
-                <input type="text" name='name' className='form-control' />
+                <input type="text" name='name' className='form-control' value={formData.name} onChange={handleChange} />
               </div>
               <br />
               <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <label htmlFor="details">Details : </label>&nbsp;&nbsp;
-                <input type="text" name='Details' className='form-control' />
+                <input type="text" name='details' className='form-control' value={formData.details} onChange={handleChange} />
               </div>
               <br />
               <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <label htmlFor="price">Price : </label>&nbsp;&nbsp;
-                <input type="text" name='price' className='form-control' />
+                <input type="text" name='price' className='form-control' value={formData.price} onChange={handleChange} />
               </div>
               <br />
               <div>
                 <label htmlFor="total">Total of Items: </label>&nbsp;&nbsp;
-                <input type="text" name='total' className='form-control' />
+                <input type="text" name='total' className='form-control' value={formData.total} onChange={handleChange} />
               </div>
               <div><br />
 
@@ -87,7 +138,7 @@ function addre() {
                 </div>
               </div><br />
               <div className="flex flex-col justify-center items-center">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">Submit</button>
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl" >Submit</button>
               </div>
 
             </form>
