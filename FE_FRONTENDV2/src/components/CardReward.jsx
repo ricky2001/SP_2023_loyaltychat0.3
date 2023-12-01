f6b8ac3efd30a0940c5ee97dcc8ec9d0668fe0ee
kungza0123 <6331305002@lamduan.mfu.ac.th>
2023-12-01 23:53:58 +0700

    del

--- FE_FRONTENDV2/src/components/CardReward.jsx
+++ FE_FRONTENDV2/src/components/CardReward.jsx
@@ -256,11 +256,8 @@ function CardReward() {
               </button>
             </div>
           </a><br />
-        </div>
-      ))}
-      <div>
-
-      {showPopup && (
+          <div>
+          {showPopup && (
         <div className="popup">
           <div className="relative w-full max-w-md max-h-full">
                    <div >
@@ -276,7 +273,8 @@ function CardReward() {
                </div>
         </div>
       )}
-      {showdelPopup && (
+          
+          {showdelPopup && (
         <div className="popup">
           <div className="relative w-full max-w-md max-h-full">
                    <div >
@@ -292,6 +290,45 @@ function CardReward() {
                </div>
         </div>
       )}
+
+
+          </div>
+        </div>
+      ))}
+      <div>
+
+      {/* {showPopup && (
+        <div className="popup">
+          <div className="relative w-full max-w-md max-h-full">
+                   <div >
+                       <div className="p-6 text-center">
+                           <svg aria-hidden="true" className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                           <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you Sure to  Update?</h3>
+                           <button  type="button" className="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2" onClick={handleUpdateReward}>
+                               Yes, I'm sure
+                           </button>
+                           <button type="button" className="bg-red-500 hover:bg-red-700 text-white font-bold py-0 px-2 rounded-xl" onClick={handleCancellation}>No, cancel</button>
+                       </div>
+                   </div>
+               </div>
+        </div>
+      )} */}
+      {/* {showdelPopup && (
+        <div className="popup">
+          <div className="relative w-full max-w-md max-h-full">
+                   <div >
+                       <div className="p-6 text-center">
+                           <svg aria-hidden="true" className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                           <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you Sure to  Delete?</h3>
+                           <button  type="button" className="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2" onClick={() => handleDeleteReward(item.itemid)}>
+                               Yes, I'm sure
+                           </button>
+                           <button   className="bg-red-500 hover:bg-red-700 text-white font-bold py-0 px-2 rounded-xl" onClick={Cancellation}>No, cancel</button>
+                       </div>
+                   </div>
+               </div>
+        </div>
+      )} */}
       {/* {showexPopup && (
         <div className="popup">
           <div className="relative w-full max-w-md max-h-full">
